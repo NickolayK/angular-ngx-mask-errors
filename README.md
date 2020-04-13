9d7057963a0da83f1e4f814eee34b8e84087acd6
NickolayK <n7412440@gmail.com>
2020-04-13 11:25:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wmxxpk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wmxxpk)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, NgxMaskModule.forRoot(options) ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
